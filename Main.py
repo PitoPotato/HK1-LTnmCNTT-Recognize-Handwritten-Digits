3eba56779501605198d98f16ec2f17955c8520b1
Pitomon <Pitomoncute@gmail.com>
2024-12-24 15:17:06 +0700

    24/12/2024
    update

--- Main.py
+++ Main.py
@@ -20,28 +20,6 @@ x_test, y_test = InOut.load_mnist(r"Databases/", kind="t10k")
  
 x_train = x_train.astype(np.float32) / 255
 x_test = x_test.astype(np.float32) / 255
- 
-# # Cách 1: Vectorization 
-# x_train = Process.vectorization(x_train)
-# x_test = Process.vectorization(x_test)
- 
-# # Cách 2: Average Pooling
-# #x_train = Process.AvergagePooling(x_train)
-# #x_test = Process.AvergagePooling(x_test)
-
-# # Cách 3: Histogram 
-# # x_train_256 = Process.calculate_histogram(x_train)
-# # x_test_256 = Process.calculate_histogram(x_test)
- 
-
-# y_pred = Process.knn(x_train, y_train, x_test[:50], Globals.Nearest)
-
-
-# accuracy = accuracy_score(y_test[:50], y_pred)
-
-# print(f"Độ chính xác trên 50 mẫu test đầu tiên: {accuracy * 100:.2f}%")
- 
-# InOut.display_images_with_predictions(x_test, y_test, y_pred, 28)
 
 x_train1 = Process.vectorization(x_train)
 x_train2 = Process.AveragePooling(x_train)
@@ -59,13 +37,13 @@ accuracies3 = []
 print("Starting KNN evaluations...")
 for k in k_values:
     print(f"Evaluating for k={k}...")
-    y_pred1 = Process.new_knn(x_train1, y_train, x_test1[:100], k)
-    y_pred2 = Process.new_knn(x_train2, y_train, x_test2[:100], k)
-    y_pred3 = Process.new_knn(x_train3, y_train, x_test3[:100], k)
+    y_pred1 = Process.new_knn(x_train1, y_train, x_test1, k)
+    y_pred2 = Process.new_knn(x_train2, y_train, x_test2, k)
+    y_pred3 = Process.new_knn(x_train3, y_train, x_test3, k)
  
-    acc1 = (y_pred1 == y_test[:100]).mean() * 100
-    acc2 = (y_pred2 == y_test[:100]).mean() * 100
-    acc3 = (y_pred3 == y_test[:100]).mean() * 100
+    acc1 = (y_pred1 == y_test).mean() * 100
+    acc2 = (y_pred2 == y_test).mean() * 100
+    acc3 = (y_pred3 == y_test).mean() * 100
  
     accuracies1.append(acc1)
     accuracies2.append(acc2)
@@ -80,7 +58,7 @@ plt.plot(k_values, accuracies3, marker='^', linestyle=':', color='g', label="His
  
 plt.xlabel("k (Number of Neighbors)", fontsize=12)
 plt.ylabel("Accuracy (%)", fontsize=12)
-plt.title("Accuracy of 100 first x_test.", fontsize=14)
+plt.title("Accuracy of modal.", fontsize=14)
  
  
 plt.grid(True, linestyle='--', alpha=0.7)
